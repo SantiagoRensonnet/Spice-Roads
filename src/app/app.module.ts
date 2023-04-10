998344ea710d64c48776b40c38be4ba4b8f4c45a
Santiago <santirensonnet@gmail.com>
2023-04-10 13:26:14 +0200

    First Commit *create general component layout *add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { RecipesComponent } from './routes/recipes/recipes.component';
+import { RecipeListComponent } from './routes/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './routes/recipes/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './routes/recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './routes/shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './routes/shopping/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
